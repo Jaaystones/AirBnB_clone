7dafb47f5ea471337a3ed2d20541380f1b0b0168
Ossai Cherish Nneka <cherishlyte13@gmail.com>
2023-02-11 06:22:02 -0800

    change

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -83,8 +83,11 @@ class TestAmenity_instantiation(unittest.TestCase):
     def test_instantiation_with_None_kwargs(self):
         with self.assertRaises(TypeError):
             Amenity(id=None, created_at=None, updated_at=None)
+
+
 class TestAmenity_save(unittest.TestCase):
     """Unittests for testing save method of the Amenity class."""
+
     @classmethod
     def setUp(self):
         try:
